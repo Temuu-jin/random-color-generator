ff0a4a34bbc6f7a1faa4087ea3ef771d30805365
Temuu-jin <temucin.d.k@gmail.com>
2023-09-05 16:22:17 +0100

    Add Comments to easily understand the code

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
+/*
+declaring const which are used in the program such as:
+inputColor - the Color that the user prompted, if no color prompted random color
+textColor - String Code of the Hex saved in a Const to use in painting the drawn lines when console.log()
+*/
 const inputColor = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
@@ -8,12 +13,30 @@ const inputColor = randomColor({
 
 const textColor = chalk.hex(inputColor);
 
-// create the drawn lines that will be colored
+/*
+Create the each Line Type that needs to be drawn
+using .repeat(i) to make code smaller
+One Line full of "#"
+One Line with "#" and " "
+One Line with "#", " " and the inputColor HexCode as String
+*/
 const fullLine = '#'.repeat(31);
 const spacedLine = '#'.repeat(5) + ' '.repeat(21) + '#'.repeat(5);
 const textLine =
   '#'.repeat(5) + ' '.repeat(7) + inputColor + ' '.repeat(7) + '#'.repeat(5);
 
+/*
+Draw "#" picture on 9 lines.
+  ###############################
+  ###############################
+  ###############################
+  #####                     #####
+  #####       #edff28       #####
+  #####                     #####
+  ###############################
+  ###############################
+  ###############################
+*/
 console.log(textColor(fullLine));
 console.log(textColor(fullLine));
 console.log(textColor(fullLine));
